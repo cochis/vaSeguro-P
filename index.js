2fe942becc7c258f0af929ed8c20c0228779ea3e
Oscar Alejandro Ramirez Rosas <ing.oarrs@gmail.com>
2019-05-22 12:35:04 -0500

    codigo de verificacion

--- index.js
+++ index.js
@@ -2,18 +2,17 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 5555;
+var port = 3800;
 mongoose.Promise = global.Promise;
 //conexion db
-mongoose.connect('mongodb://localhost:27017/db_vaseguro', {useNewUrlParser: true})
-    .then(()=> {
+mongoose.connect('mongodb://localhost:27017/db_vaseguro', { useNewUrlParser: true })
+    .then(() => {
         console.log('la conexion a la base de datos se realizo');
 
         //creando servidor
-        app.listen(port, ()=> {
+        app.listen(port, () => {
             console.log(`Servidor corriendo correctamente en el puerto ${port}`);
 
         });
     })
     .catch(err => console.log(err));
-
